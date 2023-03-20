830e50727b7ed92ebd077da8cc8e600c27bf388b
sameera <sameerak1203@icloud.com>
2023-03-20 15:13:41 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet
+ * main - Prints the alphabet.
  *
  * Return: Always 0 (Success)
  */
@@ -14,6 +14,6 @@ int main(void)
 	{
 		putchar(alp[i]);
 	}
-	puchar('\n');
+	putchar('\n');
 	return (0);
 }
