017d6a54233df528c024a4a8e31b09141aef3fc6
sameera <sameerak1203@icloud.com>
2023-03-21 13:26:52 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+  2 #define main_h
+  3 
+  4 int _putchar(char);
+  5 void print_alphabet(void);
+  6 void print_alphabet_x10(void);
+  7 int _islower(int c);
+  8 int _isalpha(int c);
+  9 int print_sign(int n);
+ 10 int _abs(int);
+ 11 int print_last_digit(int);
+ 12 void jack_bauer(void);
+ 13 void times_table(void);
+ 14 int add(int, int);
+ 15 void print_to_98(int n);
+ 16 void print_times_table(int n);
+ 17 
+ 18 #endif
