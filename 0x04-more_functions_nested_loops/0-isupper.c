ee3c99b458886285fe0c678471453c611ed63bdd
sameera <sameerak1203@icloud.com>
2023-03-24 13:52:05 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ * 
+ * Return: 0 or 1
+ */
+
+int _is upper(int c)
+{
+	if (c >= 'A' && C <= 'Z')
+		return (1);
+	else
+		return (0);
+}
